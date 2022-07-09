cd63660b93568fbeaeda8235f1c926bd02d6dccc
Bonzerbay <bonzerbay@gmail.com>
2022-07-09 12:33:19 +0100

    changed theme

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Vimqfin',
       theme: ThemeData(
        
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
       home: const MyHomePage(title: 'Home'),
     );
